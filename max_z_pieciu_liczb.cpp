65990ed2a4ca2f0c0e2917950d0ad73a40d8830b
Marek Z <UnderscoreF17@gmail.com>
2023-08-02 00:28:09 +0200

    Add files via upload

---
+++ max_z_pieciu_liczb.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	long long n[5];
+	int i;
+	for(i=0;i<5;++i)
+		cin>>n[i];
+	cout<<max(n[0],n[4]);
+	return 0;
+}
